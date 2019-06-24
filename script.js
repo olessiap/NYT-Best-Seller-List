ebb2292b6a4f2720d8c3cdd38186a7d13aee72d8
olessiap <olessia.potapova@gmail.com>
2019-06-24 12:54:07 -0300

    rendering all elements of each bookItem

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 let showBooks = function() {
-    let xhr = new XMLHttpRequest;
+    let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
-
         if(xhr.readyState !== 4) return;
-        
+
         if(xhr.status >= 200 && xhr.status < 300) {
-            let data = JSON.parse(xhr.responseText);
-            let obj = data.results.books;
-            obj.forEach(book => showBooks(book));
+            let data = JSON.parse(xhr.responseText).results.books;
+            console.log(data);
+            data.forEach(book => renderBookCard(book));
         } else {
             console.log("response failed");
         }
     }
+
     xhr.open("GET", "https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction?api-key=nvVA1COinjfya27rFtr6XudTPJ8Oz9fq");
     xhr.send();
-    const showBooks = book => {
-        //create DOM elments
-        const div = document.createElement('div');
-        const h4 = document.createElement('h4');
-        const a = document.createElement('a');
-        const img = document.createElement('img');
 
-        //append elements into the DOM
-        const body = document.querySelector('body');
-        body.append(div);
-        h4.append(a);
-        div.append(h4);
+    const renderBookCard = book => {
+        //create DOM elements
+        const div = document.createElement("div");
+        const rank = document.createElement("h1");
+        const img = document.createElement("img");
+        const weeks = document.createElement("p");
+        const title = document.createElement("h4");
+        const author = document.createElement("p");
+        const buyButton = document.createElement("button");
+
+
+        //append to DOM
+        const list = document.querySelector(".list");
+        list.append(div);
+        div.append(rank);
         div.append(img);
+        div.append(weeks);
+        div.append(title);
+        div.append(author);
+        div.append(buyButton);
 
-        //set content and attributes
-        a.innerHTML = book.title;
-        a.setAttribute('href', book.amazon_product_url);
+
+        //set data and attributes
+        div.setAttribute('class', 'bookCard');
+        rank.innerHTML = book.rank;
         img.setAttribute('src', book.book_image);
-        div.setAttribute('class', "card");
-
+        img.setAttribute('class', 'book-image');
+        weeks.innerHTML = book.weeks_on_list + " WEEKS ON THE LIST";
+        title.innerHTML = book.title;
+        author.innerHTML = "by " + book.author;
+        buyButton.innerHTML = "BUY"
     }
 }
 
