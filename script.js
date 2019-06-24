32663e050a3c99b495fb97856d4cab97443687f2
olessiap <olessia.potapova@gmail.com>
2019-06-24 11:44:35 -0300

    rendering book cards dynamically

--- script.js
+++ script.js
@@ -4,21 +4,38 @@ let showBooks = function() {
     xhr.onreadystatechange = function() {
 
         if(xhr.readyState !== 4) return;
-
+        
         if(xhr.status >= 200 && xhr.status < 300) {
             let data = JSON.parse(xhr.responseText);
-            data.results.books.forEach(function(book){
-                let bookItem = document.createElement('li');
-                bookItem.textContent = book.title;
-                let app = document.querySelector("ol");
-                app.appendChild(bookItem);
-            });
+            let obj = data.results.books;
+            obj.forEach(book => showBooks(book));
         } else {
             console.log("response failed");
         }
     }
     xhr.open("GET", "https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction?api-key=nvVA1COinjfya27rFtr6XudTPJ8Oz9fq");
     xhr.send();
+    const showBooks = book => {
+        //create DOM elments
+        const div = document.createElement('div');
+        const h4 = document.createElement('h4');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        //append elements into the DOM
+        const body = document.querySelector('body');
+        body.append(div);
+        h4.append(a);
+        div.append(h4);
+        div.append(img);
+
+        //set content and attributes
+        a.innerHTML = book.title;
+        a.setAttribute('href', book.amazon_product_url);
+        img.setAttribute('src', book.book_image);
+        div.setAttribute('class', "card");
+
+    }
 }
 
 showBooks();
